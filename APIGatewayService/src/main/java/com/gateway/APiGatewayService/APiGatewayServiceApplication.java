2500d6220fb0dcc2d2b78c4a96baf2d757e60efc
Akshay Srivastava <150348155+akshay-sri@users.noreply.github.com>
2023-12-20 16:31:43 +0530

    Add files via upload

---
+++ APIGatewayService/src/main/java/com/gateway/APiGatewayService/APiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.APiGatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APiGatewayServiceApplication.class, args);
+	}
+
+}
